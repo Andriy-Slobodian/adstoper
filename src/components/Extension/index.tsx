7a2382ef38f6ef22f9c3ff33483dc74454935a30
Andriy Slobodian <a.v.slobodyan@gmail.com>
2023-04-25 23:46:05 +0200

    Add CSS-styles & text for main Popup

--- src/components/Extension/index.tsx
+++ src/components/Extension/index.tsx
@@ -3,7 +3,13 @@ import css from './Extension.css';
 export const Extension = () => {
   return (
     <div className={css.container}>
-      <h1>Hello from my React-based Extension!</h1>
+      <h1 className={css.header}>Stop Ads</h1>
+      <p className={css.text}>Привіт. Я - маленьке розширення до браузера, призване виконувати наступне:</p>
+      <ul className={css.list}>
+        <li>Невпинно, нещадно і безжалісно <b>блокувати рекламу</b> в кожній вкладці твого браузера</li>
+        <li><b>Збільшити швидкість</b> загрузки сторінки</li>
+        <li><b>Економити Мегабайти</b> твого інтернету, котрі ти раніше витрачав на загрузку рекламних банерів</li>
+      </ul>
     </div>
   );
 };
