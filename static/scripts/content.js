d524a3ca0c99e6e927896114c1312e91d22581b6
Andriy Slobodian <andriy.slobodian@banzai.io>
2024-06-08 17:32:26 +0200

    Remove more redundant blocks from DOM

--- static/scripts/content.js
+++ static/scripts/content.js
@@ -6,18 +6,100 @@ window.onload = () => {
   if (host.includes('youtube')) {
     (() => {
       const timeout = setInterval(() => {
-        // Remove Ad section One near the video
-        const adSectionOne = document.getElementById('fulfilled-layout') || null;
-        if (adSectionOne) {
-          adSectionOne.remove();
-          console.log('EXTENSION => Ad section 1 was removed ....');
+        // Remove Ticker container
+        const ticker = document.body.querySelector('#content #masthead-container #ticker') || null;
+        if (ticker) {
+          ticker.style.display = 'none';
+          ticker.remove()
+          console.log('EXTENSION => Ticker removed')
         }
 
-        // Remove Ad section Two near the video
+        // Remove Interstitial container
+        const interstitial = document.body.querySelector('#content #masthead-container #interstitial') || null;
+        if (interstitial) {
+          interstitial.style.display = 'none';
+          interstitial.remove()
+          console.log('EXTENSION => Interstitial removed')
+        }
+
+        // Remove Alerts container
+        const alerts = document.getElementById('alerts') || null;
+        if (alerts) {
+          alerts.style.display = 'none';
+          alerts.remove()
+          console.log('EXTENSION => Alerts removed')
+        }
+
+        // Remove Messages container
+        const messages = document.getElementById('messages') || null;
+        if (messages) {
+          messages.style.display = 'none';
+          messages.remove()
+          console.log('EXTENSION => Messages removed')
+        }
+
+        // Remove ClarifyBox container
+        const clarifyBox = document.getElementById('clarify-box') || null;
+        if (clarifyBox) {
+          clarifyBox.style.display = 'none';
+          clarifyBox.remove()
+          console.log('EXTENSION => ClarifyBox removed')
+        }
+
+        // Remove LimitedState container
+        const limitedState = document.getElementById('limited-state') || null;
+        if (limitedState) {
+          limitedState.style.display = 'none';
+          limitedState.remove()
+          console.log('EXTENSION => LimitedState removed')
+        }
+
+        // Remove TicketShelf container
+        const ticketShelf = document.getElementById('ticket-shelf') || null;
+        if (ticketShelf) {
+          ticketShelf.style.display = 'none';
+          ticketShelf.remove()
+          console.log('EXTENSION => TicketShelf removed')
+        }
+
+        // Remove Secondary => Donation Shelf container
+        const donationShelf = document.getElementById('donation-shelf') || null;
+        if (donationShelf) {
+          donationShelf.style.display = 'none';
+          donationShelf.remove()
+          console.log('EXTENSION => Donation Shelf removed')
+        }
+
+        // Remove Secondary => Player Ads container
+        const playerAds = document.getElementById('player-ads') || null;
+        if (playerAds) {
+          playerAds.style.display = 'none';
+          playerAds.remove()
+          console.log('EXTENSION => Player Ads removed')
+        }
+
+        // Remove Secondary => Offer Module container
+        const offerModule = document.getElementById('offer-module') || null;
+        if (offerModule) {
+          offerModule.style.display = 'none';
+          offerModule.remove()
+          console.log('EXTENSION => Offer Module removed')
+        }
+
+        // Remove Secondary => Fulfilled Layout container
+        const fulfilledLayout = document.querySelector('#related #items #contents #fulfilled-layout') || null;
+        if (fulfilledLayout) {
+          fulfilledLayout.style.display = 'none';
+          fulfilledLayout.remove()
+          console.log('EXTENSION => Fulfilled Layout removed')
+        }
+
+        // Remove Secondary => Companion container
         const adSectionTwo = document.getElementById('companion') || null;
         if (adSectionTwo) {
+          adSectionTwo.style.display = 'none';
           adSectionTwo.remove();
-          console.log('EXTENSION => Ad section 2 was removed ....');
+          console.log('EXTENSION => Companion removed');
         }
 
         // Press "Skip ad" button over the video
@@ -53,6 +135,8 @@ window.onload = () => {
           if (noThanksBtn) {
             noThanksBtn.click()
             console.log('EXTENSION => Premium Account Modal => "Ні, дякую"');
+
+            // TODO: Remove the whole Modal from DOM instead of click()
           }
         }
 
